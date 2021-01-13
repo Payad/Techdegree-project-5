a98e4141f954ff6f85dd2c112d570fa59aaf588e
Peter Ayad <swordoflight8847@yahoo.com>
2021-01-13 14:52:32 -0800

    Update proj 5

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ baguetteBox.run('.list');
 // Code Snippet from //
 // https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/ //
 function search_photo() {
-    let input = document.getElementById('searchbar').value
+    let input = document.getElementById('searchbar').value;
     input=input.toLowerCase();
     let photo = document.getElementsByClassName('photos');
 
