f16237481bca12f196389c56fc7fff70e76b2bf8
Peter Ayad <swordoflight8847@yahoo.com>
2021-01-12 16:25:49 -0800

    Update proj 5

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
-baguetteBox.run('.grid');
-const search = new Filter('search', 'data-caption');
+baguetteBox.run('.list');
+
+
+// Code Snippet from //
+// https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/ //
+function search_photo() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let photo = document.getElementsByClassName('photos');
+
+    for(i = 0; 1 < photo.length; i++) {
+        if (!photo[i].innerHTML.toLowerCase().includes(input)) {
+            photo[i].style.display='none';
+        } else {
+            photo[i].style.display='list-item';
+        }
+    }
+}
